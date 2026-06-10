3f2a6171f4d38478049dab45e9d7928b25641ef4
Alkkaa21 <alkaroy33@gmail.com>
2026-06-10 22:37:17 +0530

    Add Event serializer and API view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+from .models import Event
+from .serializers import EventSerializer
+
+
+class EventListView(APIView):
+    def get(self, request):
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
